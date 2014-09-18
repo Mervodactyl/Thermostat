4ccb2a57e25b7f4e993ce36e1b247531a4c3e94d
Mervodactylgit config --global user.name Mervodactylgit config --global user.name Mervodactyl <merve.silk@gmail.com>
2014-09-18 17:39:18 +0100

    power saving mode tests passing

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -19,7 +19,39 @@ describe("Thermostat", function() {
 
     it('should be able to decrease temperature', function() {
       thermostat.decreaseTemperature();
-      expect(thermostat.temperature).toEqual(19)
+      expect(thermostat.temperature).toEqual(19);
+    });
+
+    it('should not be able to go below 10 degrees celcius', function() {
+      thermostat.temperature = 10;
+      thermostat.decreaseTemperature();
+      expect(thermostat.temperature).toEqual(10);
+    });
+  });
+
+  describe('power saving mode (psm)', function() {
+    it('is on by default', function() {
+      expect(thermostat.powerSavingMode).toBe(true);
+    });
+
+    it('when psm is on, maximum temp is 25 degrees', function() {
+      thermostat.temperature = 25;
+      thermostat.increaseTemperature();
+      expect(thermostat.temperature).toEqual(25);
+    });
+
+    it('when psm is off, maximum temp is 32 degrees', function() {
+      thermostat.powerSavingMode = false
+      thermostat.temperature = 32;
+      thermostat.increaseTemperature();
+      expect(thermostat.temperature).toEqual(32);
+    });
+
+    it('can raise the temperature above 25 degrees when psm is off', function() {
+      thermostat.powerSavingMode = false
+      thermostat.temperature = 25;
+      thermostat.increaseTemperature();
+      expect(thermostat.temperature).toEqual(26);
     });
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,11 +1,15 @@
 function Thermostat(){
   this.temperature = 20;
+  this.minimumTemperature = 10;
+  this.powerSavingMode = true;
 };
 
 Thermostat.prototype.increaseTemperature = function() {
-  this.temperature += 1
+  if(this.powerSavingMode === true && this.temperature < 25) {this.temperature += 1};
+  if(this.powerSavingMode === false && this.temperature < 32) {this.temperature += 1};
 };
 
 Thermostat.prototype.decreaseTemperature = function() {
-  this.temperature -=1
+  if(this.temperature > 10) {this.temperature -= 1};
 };
+
